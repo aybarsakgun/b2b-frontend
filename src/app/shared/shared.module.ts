985e9bd96d5515151bedf60b590d80784b4bd9ef
Aybars AKGÜN <aybars.akgun77@gmail.com>
2021-05-23 21:45:10 +0300

    Lazy load translations doesnt load on lazy loaded modules issue fixed

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,16 +1,24 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
-import {TranslateModule} from '@ngx-translate/core';
+import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
 import {ReactiveFormsModule} from '@angular/forms';
 import {LanguageSelectionComponent} from './components/language-selection/language-selection.component';
 import {ClickOutsideDirective} from './directives/click-outside.directive';
 import {PaginationComponent} from './components/pagination/pagination.component';
+import {HttpClient} from '@angular/common/http';
+import {TranslationLoaderFactory} from './loaders/translation.loader';
 
 @NgModule({
   imports: [
     CommonModule,
     ReactiveFormsModule,
-    TranslateModule.forChild(),
+    TranslateModule.forChild({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: TranslationLoaderFactory,
+        deps: [HttpClient],
+      },
+    }),
   ],
   declarations: [
     LanguageSelectionComponent,
