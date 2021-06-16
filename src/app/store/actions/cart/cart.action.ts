91b7ba0dc8855029af679d65d4dfe8292dc94508
Aybars AKGÜN <aybars.akgun77@gmail.com>
2021-06-17 01:27:18 +0300

    Some improvements on cart state

--- src/app/store/actions/cart/cart.action.ts
+++ src/app/store/actions/cart/cart.action.ts
@@ -3,26 +3,47 @@ import {ProductModel} from '../../../models/product/product.model';
 import {ProductUnitModel} from '../../../models/product/product-unit.model';
 
 export namespace Cart {
+  export enum CartOperationType {
+    FETCH = '[Cart] Fetch',
+    FETCH_FAILED = '[Cart] Fetch Failed',
+    FETCH_SUCCESS = '[Cart] Fetch Success',
+    ADD_ITEM = '[Cart] Add Item',
+    ADD_ITEM_FAILED = '[Cart] Add Item Failed',
+    ADD_ITEM_SUCCESS = '[Cart] Add Item Success',
+    REMOVE_ITEM = '[Cart] Remove Item',
+    REMOVE_ITEM_FAILED = '[Cart] Remove Item Failed',
+    REMOVE_ITEM_SUCCESS = '[Cart] Remove Item Success',
+    CHANGE_QUANTITY = '[Cart] Change Quantity',
+    CHANGE_QUANTITY_FAILED = '[Cart] Change Quantity Failed',
+    CHANGE_QUANTITY_SUCCESS = '[Cart] Change Quantity Success',
+    CHANGE_UNIT = '[Cart] Change Unit',
+    CHANGE_UNIT_FAILED = '[Cart] Change Unit Failed',
+    CHANGE_UNIT_SUCCESS = '[Cart] Change Unit Success',
+    CLEAR = '[Cart] Clear',
+    CLEAR_FAILED = '[Cart] Clear Failed',
+    CLEAR_SUCCESS = '[Cart] Clear Success',
+  }
+
   export class Fetch {
-    static readonly type = '[Cart] Fetch';
+    static readonly type = CartOperationType.FETCH;
   }
 
   export class FetchFailed {
-    static readonly type = '[Cart] Fetch Failed';
+    static readonly type = CartOperationType.FETCH_FAILED;
 
     constructor(public errors: string[]) {
     }
   }
 
   export class FetchSuccess {
-    static readonly type = '[Cart] Fetch Success';
+    static readonly type = CartOperationType.FETCH_SUCCESS;
 
     constructor(public cart: CartModel[]) {
     }
   }
 
-  export class AddProduct {
-    static readonly type = '[Cart] Add Product';
+  export class AddItem {
+    static readonly type = CartOperationType.ADD_ITEM;
 
     constructor(public payload: {
       product: ProductModel,
@@ -31,8 +52,22 @@ export namespace Cart {
     }
   }
 
-  export class RemoveProduct {
-    static readonly type = '[Cart] Remove Product';
+  export class AddItemFailed {
+    static readonly type = CartOperationType.ADD_ITEM_FAILED;
+
+    constructor(public errors: string[]) {
+    }
+  }
+
+  export class AddItemSuccess {
+    static readonly type = CartOperationType.ADD_ITEM_SUCCESS;
+
+    constructor(public cart: CartModel[]) {
+    }
+  }
+
+  export class RemoveItem {
+    static readonly type = CartOperationType.REMOVE_ITEM;
 
     constructor(public payload: {
       product: ProductModel
@@ -40,8 +75,22 @@ export namespace Cart {
     }
   }
 
+  export class RemoveItemFailed {
+    static readonly type = CartOperationType.REMOVE_ITEM_FAILED;
+
+    constructor(public errors: string[]) {
+    }
+  }
+
+  export class RemoveItemSuccess {
+    static readonly type = CartOperationType.REMOVE_ITEM_SUCCESS;
+
+    constructor(public cart: CartModel[]) {
+    }
+  }
+
   export class ChangeQuantity {
-    static readonly type = '[Cart] Change Quantity';
+    static readonly type = CartOperationType.CHANGE_QUANTITY;
 
     constructor(public payload: {
       product: ProductModel,
@@ -50,8 +99,22 @@ export namespace Cart {
     }
   }
 
+  export class ChangeQuantityFailed {
+    static readonly type = CartOperationType.CHANGE_QUANTITY_FAILED;
+
+    constructor(public errors: string[]) {
+    }
+  }
+
+  export class ChangeQuantitySuccess {
+    static readonly type = CartOperationType.CHANGE_QUANTITY_SUCCESS;
+
+    constructor(public cart: CartModel[]) {
+    }
+  }
+
   export class ChangeUnit {
-    static readonly type = '[Cart] Change Unit';
+    static readonly type = CartOperationType.CHANGE_UNIT;
 
     constructor(public payload: {
       product: ProductModel,
@@ -60,7 +123,35 @@ export namespace Cart {
     }
   }
 
+  export class ChangeUnitFailed {
+    static readonly type = CartOperationType.CHANGE_UNIT_FAILED;
+
+    constructor(public errors: string[]) {
+    }
+  }
+
+  export class ChangeUnitSuccess {
+    static readonly type = CartOperationType.CHANGE_UNIT_SUCCESS;
+
+    constructor(public cart: CartModel[]) {
+    }
+  }
+
   export class Clear {
-    static readonly type = '[Cart] Clear';
+    static readonly type = CartOperationType.CLEAR;
+  }
+
+  export class ClearFailed {
+    static readonly type = CartOperationType.CLEAR_FAILED;
+
+    constructor(public errors: string[]) {
+    }
+  }
+
+  export class ClearSuccess {
+    static readonly type = CartOperationType.CLEAR_SUCCESS;
+
+    constructor(public cart: CartModel[]) {
+    }
   }
 }
